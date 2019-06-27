c0a73e21260d31770998f84a836cca85069e9826
zhen chen <chenzhen@aloestec.com>
2019-06-27 21:30:18 +0800

    增加vm代理&created

--- Tvue.js
+++ Tvue.js
@@ -1,21 +1,28 @@
 class Tvue {
-  constructor(options) {  // new Tvue里的值
+  constructor(options, vm) {  // new Tvue里的值
     this.$options = options
     this._data = options.data // 参考源码，避免与data重名
     // 增加修改数据功能 进行数据劫持 数据劫持依赖发布订阅者模式
-    this.observer(this._data)
+    this.observer(this._data)  // 观察者
     this.compile(options.el)  // 查找替换
+    if (options.created) { // 注册使用created
+      options.created.call(this)
+    }
   }
   observer(data) {
+    if (!data) return
     Object.keys(data).forEach(key => {
       let value = data[key]
-      let dep = new Dep() // 实例化发不器
+      let dep = new Dep() // 实例化发不器   依赖收集器构造函数
+      // dep与key是一一对应的关系，每个key都会生成新的watcher
+      console.log(dep, 'dep---dep14')
       Object.defineProperty(data, key, {
         configurable: true,  // 可配置
         enumerable: true, // 可枚举
         get() { //一旦目标属性被访问就会调回此方法， 并将此方法的运算结果返回用户。
           if (Dep.target) {  
-            dep.addSub(Dep.target)  // 注册
+            console.log(Dep.target, 'Dep.target')
+            dep.addDep(Dep.target)  // 注册  维护dep里的若干个watcher
           }
           return value;
         },
@@ -24,17 +31,46 @@ class Tvue {
           if (newValue !== value)
           value = newValue
           dep.notify(newValue)
-        }
+        } 
       })
+      this.proxyData(key); // 将vm._data.message代理为vm.message
+    })
+  }
+  proxyData(key) {
+    Object.defineProperty(this, key , {
+      get() {
+        return this._data[key]
+      },
+      set(newValue) {
+        this._data[key] = newValue
+      }
     })
   }
   compile(el) {
+    // this.$vm = vm;
     //  作用域 范围内查找  
-    let element = document.querySelector(el)  // 获取<div id="app"></div>
-    this.compileNode(element)
+    this.$el = document.querySelector(el)  // 获取<div id="app"></div>
+    if (this.$el) {
+      // 提取宿主环境中模版内容到Fragment标签，提升dom操作效率
+      this.$fragment = this.node2Fragment(this.$el)
+      console.log(this.$fragment, this.$el, '53---53')
+      // 编译模版内容，同时进行依赖收集
+      this.compileNode(this.$fragment)
+      this.$el.appendChild(this.$fragment)
+      // this.compileNode(this.$el)
+    }
   }
-  compileNode(element) { // 递归实现多层嵌套查找替换
-    let childNodes = element.childNodes // 获取子节点
+  node2Fragment(el) {
+    const fragment = document.createDocumentFragment()  // 创建文档片段，附加到dom树中
+    console.log(fragment, 'fragment---fragment--62')
+    let child;
+    while(child = el.firstChild) {
+      fragment.appendChild(child)
+    }
+    return fragment
+  }
+  compileNode(el) { // 递归实现多层嵌套查找替换
+    let childNodes = el.childNodes // 获取子节点
     Array.from(childNodes).forEach((node) => {
       // console.log(node, 'nodeType 属性可用来区分不同类型的节点，比如 元素, 文本 和 注释。')
       if (node.nodeType === 3) {
@@ -69,7 +105,7 @@ class Tvue {
         })
 
       }
-      if (node.childNodes.length > 0) {
+      if (node.childNodes && node.childNodes.length > 0) {
         this.compileNode(node)
       }
     })
@@ -77,19 +113,19 @@ class Tvue {
 }
 class Dep {
   constructor() {
-    this.subs = []
+    this.deps = []
   }
-  addSub(sub) {
-    this.subs.push(sub)
+  addDep(dep) {
+    this.deps.push(dep)
   }
   notify(newValue) {
-    this.subs.forEach((v) => {
-      v.updata(newValue)
+    this.deps.forEach((dep) => {
+      dep.updata(newValue)
     })
   }
 }
 
-class Watcher {
+class Watcher {  // 订阅者
   constructor(vm,exp,cb) {
     console.log(vm._data, 'vm')
     console.log(exp, vm._data[exp], 'exp') // message 测试
